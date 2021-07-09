c7239178a01f8aa21924e2fb016fc561b6e74cda
shenying2 <shenying2@lenovo.com>
2021-07-09 18:03:42 +0800

    driver/usb: wusb3801 sometimes cc orientation recognize issue
    connect then disconnect otg, insert usb cable in cc2 direction, recognize as
    cc1, just for once.
    rootcause: wusb3801 need to turn off vbus right after detach otg, but current
    tcpc code will take about 32ms to turn off vbus. In this situation, wusb will
    get several abnormal irq.
    solution: sleep 100ms in detach will reduce abnormal irq.
    
    Change-Id: I1067f87f37015ca7500467f8ee5c190d53ffc12c
    Signed-off-by: shenying2 <shenying2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2008126
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shengchun Li <lisc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/typec/tcpc/tcpc_wusb3801.c
+++ drivers/misc/mediatek/typec/tcpc/tcpc_wusb3801.c
@@ -277,7 +277,7 @@ static void wusb3801_irq_work_handler(struct kthread_work *work)
 	rc = wusb3801_i2c_read8(chip->tcpc, WUSB3801_REG_STATUS);
 	if (rc < 0) {
 		pr_err("%s: failed to read reg status\n", __func__);
-		return;
+		return; 
 	}
 	pr_info("%s WUSB3801_REG_STATUS : 0x%02x\n", __func__, rc);
 
@@ -302,6 +302,7 @@ static void wusb3801_irq_work_handler(struct kthread_work *work)
 		//tcpci_notify_typec_state(tcpc);
 		if (tcpc->typec_attach_old == TYPEC_ATTACHED_SRC) {
 		    tcpci_source_vbus(tcpc, TCP_VBUS_CTRL_TYPEC, TCPC_VBUS_SOURCE_0V, 0);
+			msleep(100);
 		}
 		tcpc->typec_attach_old = TYPEC_UNATTACHED;
 	}
@@ -1033,7 +1034,7 @@ static int wusb3801_i2c_probe(struct i2c_client *client,
 	}
 	chip->dev = &client->dev;
 	chip->client = client;
-#ifdef __TEST_CC_PATCH_
+#ifdef __TEST_CC_PATCH__
 	chip->cc_sts = 0xFF;
 	chip->cc_test_flag = 0;
 	chip->dev_sub_id = dev_sub_id;
