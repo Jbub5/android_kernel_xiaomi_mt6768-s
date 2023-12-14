019c7147e1a3f1d9d4475a5c89067fc93aaf0d64
fukiame <fukiame@proton.me>
2023-12-14 16:40:50 +0700

    [DNM]drivers/input: fingerprint: goodix: complete silence
    tis a squash, heres the original commit mesaages
    
    [DNM]drivers/input: fingerprint: goodix: complete silence
    [DNM]fixup!drivers/input: fingerprint/goodix: fix compile erruh
    [DNM]drivers/input: fingerprint: goodix: sipence gf_spi_clk_enable
    [DNM]drivers/input: fingerprint: goofix: ':%s/.*printk.*\n//gc'

--- drivers/input/fingerprint/goodix/gf_spi_tee.h
+++ drivers/input/fingerprint/goodix/gf_spi_tee.h
@@ -27,11 +27,8 @@
 /* debug log setting */
 extern u8 g_debug_level;
 
-#define gf_debug(level, fmt, args...) do { \
-			if (g_debug_level >= level) {\
-				pr_warn("[gf] " fmt, ##args); \
-			} \
-		} while (0)
+#define gf_debug(level, fmt, args...) { \
+}
 
 #define FUNC_ENTRY()  gf_debug(DEBUG_LOG, "%s, %d, enter\n", __func__, __LINE__)
 #define FUNC_EXIT()  gf_debug(DEBUG_LOG, "%s, %d, exit\n", __func__, __LINE__)
