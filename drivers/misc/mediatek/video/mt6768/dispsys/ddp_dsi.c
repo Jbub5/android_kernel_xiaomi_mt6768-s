6ed9edaffcd9f6fa39113fe6661769cdf82617f5
fukiame <fukiame@proton.me>
2024-09-18 11:33:17 +0700

    drivers/misc: mediatek: video: dispsys: kill dsi_analysis
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/video/mt6768/dispsys/ddp_dsi.c
+++ drivers/misc/mediatek/video/mt6768/dispsys/ddp_dsi.c
@@ -6480,6 +6480,7 @@ static const char *dsi_mode_spy(enum LCM_DSI_MODE_CON mode)
 
 void dsi_analysis(enum DISP_MODULE_ENUM module)
 {
+#if 0
 	int i = 0;
 
 	DDPDUMP("== DISP DSI ANALYSIS ==\n");
@@ -6525,6 +6526,7 @@ void dsi_analysis(enum DISP_MODULE_ENUM module)
 			i, DSI_REG[i]->DSI_LFR_CON.LFR_TYPE,
 			DSI_REG[i]->DSI_LFR_CON.LFR_SKIP_NUM);
 	}
+#endif
 }
 
 int ddp_dsi_dump(enum DISP_MODULE_ENUM module, int level)
