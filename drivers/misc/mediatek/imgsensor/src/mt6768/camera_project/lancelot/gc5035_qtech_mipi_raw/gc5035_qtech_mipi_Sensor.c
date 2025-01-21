2356aa9dd0439dab9c5c92645bee96e27fbd4077
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2025-01-21 16:05:54 +0300

    Revert "waiting hehe"
    This reverts commit 53e6b3abb3cc43a0826dcbcde0b076f08991e35b.
    
    The lancelot users reported gcam working after reverting that commit

--- drivers/misc/mediatek/imgsensor/src/mt6768/camera_project/lancelot/gc5035_qtech_mipi_raw/gc5035_qtech_mipi_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt6768/camera_project/lancelot/gc5035_qtech_mipi_raw/gc5035_qtech_mipi_Sensor.c
@@ -701,7 +701,11 @@ static void set_dummy(void)
 
 static kal_uint32 return_sensor_id(void)
 {
-	return ((read_cmos_sensor(0xf0) << 8) | read_cmos_sensor(0xf1));
+	kal_uint32 get_byte = 0;
+
+	get_byte = (read_cmos_sensor(0xf0) << 8) | read_cmos_sensor(0xf1);
+	get_byte = get_byte + 1;
+	return get_byte;
 }
 
 static void set_max_framerate(UINT16 framerate, kal_bool min_framelength_en)
--- drivers/misc/mediatek/imgsensor/src/mt6768/camera_project/lancelot/ov2180_ofilm_mipi_raw/ov2180_ofilm_mipi_raw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt6768/camera_project/lancelot/ov2180_ofilm_mipi_raw/ov2180_ofilm_mipi_raw_Sensor.c
@@ -212,7 +212,11 @@ static void set_dummy(void)
 
 static kal_uint32 return_sensor_id(void)
 {
-	return ((read_cmos_sensor(0x300A) << 8) | read_cmos_sensor(0x300B));
+	kal_uint32 get_byte = 0;
+
+	get_byte = (read_cmos_sensor(0x300A) << 8) | read_cmos_sensor(0x300B);
+	get_byte = get_byte + 3;
+	return get_byte;
 }
 
 static void set_max_framerate(UINT16 framerate, kal_bool min_framelength_en)
