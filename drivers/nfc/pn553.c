fa91f25de71b57ccce944115109c018df031d4e9
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-04-30 21:37:20 +0000

    c

--- drivers/nfc/pn553.c
+++ drivers/nfc/pn553.c
@@ -87,7 +87,9 @@ extern int force_sig_info(int, struct siginfo *, struct task_struct *);
 
 /* VEN is kept ON all the time if you define the macro VEN_ALWAYS_ON.
 Used for SN100 usecases */
-//#define VEN_ALWAYS_ON
+#ifndef CONFIG_TARGET_PRODUCT_SELENECOMMON
+#define VEN_ALWAYS_ON
+#endif
 /* Macro added to disable SVDD power toggling */
 /* #define JCOP_4X_VALIDATION */
 
