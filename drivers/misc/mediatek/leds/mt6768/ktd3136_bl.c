27225d8af92803bf21429a6938b03be87f1d4c5e
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-04-30 21:52:57 +0000

    c

--- drivers/misc/mediatek/leds/mt6768/ktd3136_bl.c
+++ drivers/misc/mediatek/leds/mt6768/ktd3136_bl.c
@@ -648,6 +648,13 @@ int ktd3137_brightness_set(int brightness)
 	//LOG_DBG("%s brightness = %d\n", __func__, brightness);
 	/* Huaqin modify for K19A-271 by caogaojie at 2021/07/08 end */
 
+#ifdef CONFIG_TARGET_PRODUCT_MERLINCOMMON
+	if ((brightness < 5) && (brightness > 2)) {//HQ-61731
+		brightness = 5;
+		printk("%s: lyd_lowest_brightness, brightness = %d", __func__, brightness);
+	}
+#endif
+
 	ktd3137_brightness_set_workfunc(bkl_chip, brightness);
 	return brightness;
 }
--- drivers/misc/mediatek/video/include/mtkfb.h
+++ drivers/misc/mediatek/video/include/mtkfb.h
@@ -319,9 +319,11 @@ struct fb_post_video_buffer {
 struct fb_lcd_merlin_para {
 	int white_point_x;
 	int white_point_y;
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
 /* Huaqin modify for HQ-126356 by caogaojie at 2021/05/06 start */
 	int white_point_l;
 /* Huaqin modify for HQ-126356 by caogaojie at 2021/05/06 end */
+#endif
 	u32 white_point_r;
 	u32 white_point_g;
 	u32 white_point_b;
