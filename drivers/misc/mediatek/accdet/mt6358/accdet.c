e3edd527ff5bf72480159151759ba3692de8fc59
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-04-30 21:29:40 +0000

    selene

--- drivers/misc/mediatek/accdet/mt6358/accdet.c
+++ drivers/misc/mediatek/accdet/mt6358/accdet.c
@@ -2397,6 +2397,10 @@ int mt_accdet_probe(struct platform_device *dev)
 	__set_bit(KEY_VOLUMEDOWN, accdet_input_dev->keybit);
 	__set_bit(KEY_VOLUMEUP, accdet_input_dev->keybit);
 	__set_bit(KEY_VOICECOMMAND, accdet_input_dev->keybit);
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	__set_bit(BTN_1, accdet_input_dev->keybit);
+	__set_bit(BTN_2, accdet_input_dev->keybit);
+#endif
 
 	__set_bit(EV_SW, accdet_input_dev->evbit);
 	__set_bit(SW_HEADPHONE_INSERT, accdet_input_dev->swbit);
