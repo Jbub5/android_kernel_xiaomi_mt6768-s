3b0ce3cbdb2db6768290b94907d5a3e27652ce32
Vrdons <vrdons@proton.me>
2026-05-16 23:10:24 +0300

    drivers/typec: tcpc_wusb3801: Guard BQ2589X_CHARGER changes

--- drivers/misc/mediatek/typec/tcpc/tcpc_wusb3801.c
+++ drivers/misc/mediatek/typec/tcpc/tcpc_wusb3801.c
@@ -394,16 +394,19 @@ void wusb3801_intr_handler_resume(void)
 static irqreturn_t wusb3801_intr_handler(int irq, void *data)
 {
 	struct wusb3801_chip *chip = data;
-	/*K19A HQHW-963 K19A for sy cdp by langjunjun at 2021/7/15 start*/
+	#ifdef CONFIG_CHARGER_BQ2589X_CHARGER
 	if (bq2589x_get_cdp_status() == true) {
 		pr_err("%s:ljj  bq2589x_get_cdp_status is true,returned!!!\n", __func__);
 		g_irq_3801_flag = true;
 		g_3801_chip = chip;
+	#else
+	if (0) {
+	#endif
 	} else {
 		__pm_wakeup_event(&chip->irq_wake_lock, WUSB3801_IRQ_WAKE_TIME);
 		kthread_queue_work(&chip->irq_worker, &chip->irq_work);
 	}
-	/*K19A HQHW-963 K19A for sy cdp by langjunjun at 2021/7/15 end*/
+
 	return IRQ_HANDLED;
 }
 
