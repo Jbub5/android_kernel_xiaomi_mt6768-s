63a34af996b4faa2f85d0d2adc592a8ac0ec55d5
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-06-03 11:57:09 +0000

    fix

--- drivers/misc/mediatek/imgsensor/src/common/v1/imgsensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1/imgsensor.c
@@ -166,7 +166,7 @@ static void imgsensor_mutex_unlock(struct IMGSENSOR_SENSOR_INST *psensor_inst)
 	else
 		mutex_unlock(&gimgsensor_mutex);
 #else
-	mutex_lock(&psensor_inst->sensor_mutex);
+	mutex_unlock(&psensor_inst->sensor_mutex);
 #endif
 }
 
